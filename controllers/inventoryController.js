7a3498ffa375ccb1d82337b56866dfa00143fb13
ChenElla <cyqfypex@gmail.com>
2023-04-15 17:02:42 -0400

    Update some route names

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,7 +1,7 @@
 const knex = require('knex')(require('../knexfile'));
 
 exports.index = (_req, res) => {
-  knex('inventory')
+  knex('inventories')
     .then((data) => {
       res.status(200).json(data);
     })
