2909214bc2bef9f0b72a2d89de78ae3a387cef05
Will <wgale4@gmail.com>
2023-04-15 18:49:33 -0400

    updated to latast version of npm. created post route for new warehouses with validation working. some inconsistencies around the email validation persist between this code and a teamate where seemingly the same variables are having different effects. updated route names in index file

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,4 +1,14 @@
-const knex = require('knex')(require('../knexfile'));
+const knex = require("knex")(require("../knexfile"));
+const uniqueID = require("uniqid");
+
+//email validation
+// const validRegex_email =
+//   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+const validRegex_email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// phone number validation
+const validRegex_phone = /\+1 \([0-9]{3}\) [0-9]{3}-[0-9]{3}/i;
 
 exports.index = (_req, res) => {
   knex("warehouses")
@@ -30,7 +40,7 @@ exports.singleWarehouse = (req, res) => {
 };
 
 exports.warehouseInventories = (req, res) => {
-  knex('inventories')
+  knex("inventories")
     .where({ warehouse_id: req.params.id })
     .then((data) => {
       res.status(200).json(data);
@@ -46,26 +56,61 @@ exports.warehouseInventories = (req, res) => {
 
 exports.addWarehouse = (req, res) => {
   // Validate the request body for required data
-  if (!req.body.name || !req.body.manager || !req.body.address || !req.body.phone || !req.body.email) {
-    return res.status(400).send('Please make sure to provide name, manager, address, phone and email fields in a request');
+  if (
+    !req.body.warehouse_name ||
+    !req.body.address ||
+    !req.body.city ||
+    !req.body.country ||
+    !req.body.contact_name ||
+    !req.body.contact_position ||
+    !req.body.contact_phone ||
+    !req.body.contact_email
+  ) {
+    return res
+      .status(400)
+      .send(
+        "Please make sure to provide name, manager, address, phone and email fields in a request"
+      );
   }
+  console.log(req.body);
+  if (!req.body.contact_email.match(validRegex_email)) {
+    return res.status(400).send("Please make sure to provide a valid email");
+  }
+  if (!req.body.contact_phone.match(validRegex_phone)) {
+    return res
+      .status(400)
+      .send("Please make sure to provide a valid phone number");
+  }
+  const newWarehouse = {
+    id: uniqueID(),
+    warehouse_name: req.body.warehouse_name,
+    address: req.body.address,
+    city: req.body.city,
+    country: req.body.country,
+    contact_name: req.body.contact_name,
+    contact_position: req.body.contact_position,
+    contact_phone: req.body.contact_phone,
+    contact_email: req.body.contact_email,
+  };
 
-  knex('warehouses')
-    .insert(req.body)
+  knex("warehouses")
+    .insert(newWarehouse)
     .then((data) => {
       // For POST requests we need to respond with 201 and the location of the newly created record
-      const newWarehouseURL = `/warehouses/${data[0]}`;
-      res.status(201).location(newWarehouseURL).send(newWarehouseURL);
+      // const newWarehouseURL = `/warehouses/${data[0]}`;
+      res.status(201).json(newWarehouse);
     })
     .catch((err) => res.status(400).send(`Error creating Warehouse: ${err}`));
 };
 
 exports.updateWarehouse = (req, res) => {
-  knex('warehouses')
+  knex("warehouses")
     .update(req.body)
     .where({ id: req.params.id })
     .then(() => {
-      res.status(200).send(`Warehouse with id: ${req.params.id} has been updated`);
+      res
+        .status(200)
+        .send(`Warehouse with id: ${req.params.id} has been updated`);
     })
     .catch((err) =>
       res.status(400).send(`Error updating Warehouse ${req.params.id} ${err}`)
@@ -73,11 +118,13 @@ exports.updateWarehouse = (req, res) => {
 };
 
 exports.updateWarehouse = (req, res) => {
-  knex('warehouses')
+  knex("warehouses")
     .update(req.body)
     .where({ id: req.params.id })
     .then(() => {
-      res.status(200).send(`Warehouse with id: ${req.params.id} has been updated`);
+      res
+        .status(200)
+        .send(`Warehouse with id: ${req.params.id} has been updated`);
     })
     .catch((err) =>
       res.status(500).send(`Error updating Warehouse ${req.params.id} ${err}`)
@@ -85,12 +132,14 @@ exports.updateWarehouse = (req, res) => {
 };
 
 exports.deleteWarehouse = (req, res) => {
-  knex('warehouses')
+  knex("warehouses")
     .delete()
     .where({ id: req.params.id })
     .then(() => {
       // For DELETE response we can use 204 status code
-      res.status(204).send(`Warehouse with id: ${req.params.id} has been deleted`);
+      res
+        .status(204)
+        .send(`Warehouse with id: ${req.params.id} has been deleted`);
     })
     .catch((err) =>
       res.status(500).send(`Error deleting Warehouse ${req.params.id} ${err}`)
