b198a4533958bd04d20af19f4f21d1c26062c411
Will <wgale4@gmail.com>
2023-04-15 16:30:39 -0400

    created route for single item in inventoryController

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,11 +1,24 @@
-const knex = require('knex')(require('../knexfile'));
+const knex = require("knex")(require("../knexfile"));
 
 exports.index = (_req, res) => {
-  knex('inventory')
+  knex("inventories")
     .then((data) => {
       res.status(200).json(data);
     })
     .catch((err) =>
-      res.status(400).send(`Error retrieving Inventories: ${err}`)
+      res.status(400).send(`Error Retrieving Inventories: ${err}`)
     );
+};
+
+exports.singleItem = (req, res) => {
+  knex("inventories")
+    .where({ id: req.params.id })
+    .then((data) => {
+      res.json(data[0]);
+    })
+    .catch((err) => {
+      res
+        .status(404)
+        .json({ message: `error getting inventory item ${req.params.id}` });
+    });
 };
