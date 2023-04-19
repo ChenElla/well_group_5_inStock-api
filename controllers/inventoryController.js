9ca6119861e34bf72dd3a711fa4ec56b11adc3c6
Will <wgale4@gmail.com>
2023-04-18 22:31:40 -0400

    added if else statement to single item get request to return 404 when id incorrect. Changed .env to rootroot password

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -16,27 +16,33 @@ exports.singleItem = (req, res) => {
     .join("warehouses", "inventories.warehouse_id", "=", "warehouses.id")
     .where("inventories.id", req.params.id)
     .then((data) => {
-        res.status(200).json(data[0]);
-      }) .catch((err) =>
+      if (!data.length)
+        res
+          .status(404)
+          .send(`message: error getting inventory item ${req.params.id}`);
+      else res.status(200).json(data[0]);
+    })
+    .catch((err) =>
       res
-      .status(404)
-      .send(`message: error getting inventory item ${req.params.id}`)
-      )};
+        .status(404)
+        .send(`message: error getting inventory item ${req.params.id}`)
+    );
+};
 
 exports.getAllInventories = async (_req, res) => {
   try {
     const inventories = await knex
       .select(
-        'inventories.id',
-        'warehouses.warehouse_name as warehouse_name',
-        'inventories.item_name',
-        'inventories.description',
-        'inventories.category',
-        'inventories.status',
-        'inventories.quantity'
+        "inventories.id",
+        "warehouses.warehouse_name as warehouse_name",
+        "inventories.item_name",
+        "inventories.description",
+        "inventories.category",
+        "inventories.status",
+        "inventories.quantity"
       )
-      .from('inventories')
-      .join('warehouses', 'inventories.warehouse_id', '=', 'warehouses.id');
+      .from("inventories")
+      .join("warehouses", "inventories.warehouse_id", "=", "warehouses.id");
 
     res.status(200).json(inventories);
   } catch (err) {
@@ -48,60 +54,61 @@ exports.getAllInventories = async (_req, res) => {
 
 exports.updateInventory = async (req, res) => {
   const { id } = req.params;
-  const {
-    warehouse_id,
-    item_name,
-    description,
-    category,
-    status,
-    quantity
-  } = req.body;
+  const { warehouse_id, item_name, description, category, status, quantity } =
+    req.body;
 
   // Validate request body data
-  if (!warehouse_id || !item_name || !description || !category || !status || quantity === undefined) {
-    return res.status(400).send('All values are required (non-empty).');
+  if (
+    !warehouse_id ||
+    !item_name ||
+    !description ||
+    !category ||
+    !status ||
+    quantity === undefined
+  ) {
+    return res.status(400).send("All values are required (non-empty).");
   }
 
   if (isNaN(quantity)) {
-    return res.status(400).send('Quantity must be a number.');
+    return res.status(400).send("Quantity must be a number.");
   }
 
   try {
     // Check if warehouse_id exists in warehouses table
-    const warehouseExists = await knex('warehouses').where('id', warehouse_id).first();
+    const warehouseExists = await knex("warehouses")
+      .where("id", warehouse_id)
+      .first();
 
     if (!warehouseExists) {
-      return res.status(400).send('Invalid warehouse_id.');
+      return res.status(400).send("Invalid warehouse_id.");
     }
 
     // Update inventory item
-    const updatedRows = await knex('inventories')
-      .where('id', id)
-      .update({
-        warehouse_id,
-        item_name,
-        description,
-        category,
-        status,
-        quantity
-      });
+    const updatedRows = await knex("inventories").where("id", id).update({
+      warehouse_id,
+      item_name,
+      description,
+      category,
+      status,
+      quantity,
+    });
 
     if (updatedRows === 0) {
-      return res.status(404).send('Inventory ID not found.');
+      return res.status(404).send("Inventory ID not found.");
     }
 
     // Return the updated inventory item without created_at and updated_at
-    const updatedInventory = await knex('inventories')
+    const updatedInventory = await knex("inventories")
       .select(
-        'id',
-        'warehouse_id',
-        'item_name',
-        'description',
-        'category',
-        'status',
-        'quantity'
+        "id",
+        "warehouse_id",
+        "item_name",
+        "description",
+        "category",
+        "status",
+        "quantity"
       )
-      .where('id', id)
+      .where("id", id)
       .first();
 
     res.status(200).json(updatedInventory);
