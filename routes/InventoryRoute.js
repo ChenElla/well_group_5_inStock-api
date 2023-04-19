9285233e3d7aadf4cc36cbfc970efad75a983edc
Will <wgale4@gmail.com>
2023-04-18 22:15:36 -0400

    deleted redendant routes comments

--- routes/InventoryRoute.js
+++ routes/InventoryRoute.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 const inventoryController = require("../controllers/inventoryController");
-// const knex = require("knex")(require("../knexfile"));
 
-// router.route("/").get(inventoryController.index);
 router.route("/").get(inventoryController.getAllInventories);
 router.put("/:id", inventoryController.updateInventory);
 
