d8fa4d703443bdc3ab8a3f890d9fa4389e6b10e4
Logan Henrie <loghenrie@gmail.com>
2023-04-20 20:05:59 -0400

    finish combination of post/put into upsert

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5050;
 
-const warehouseRoutes = require('./routes/warehouseRoute');
-const inventoryRoutes = require('./routes/InventoryRoute.js');
+const warehouseRoutes = require("./routes/WarehouseRoute.js");
+const inventoryRoutes = require("./routes/InventoryRoute.js");
 
 app.use(express.json());
-const cors = require('cors');
-app.use(cors({
-    origin: '*'
-}));
-app.use('/inventories', inventoryRoutes);
-app.use('/warehouses', warehouseRoutes);
+const cors = require("cors");
+app.use(
+	cors({
+		origin: "*",
+	})
+);
+app.use("/inventories", inventoryRoutes);
+app.use("/warehouses", warehouseRoutes);
 
 app.listen(PORT, () => {
-  console.log(`running at http://localhost:${PORT}`);
+	console.log(`running at http://localhost:${PORT}`);
 });
--- routes/InventoryRoute.js
+++ routes/InventoryRoute.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 const inventoryController = require("../controllers/inventoryController");
 // const knex = require("knex")(require("../knexfile"));
 
-router.route("/").get(inventoryController.index);
+// router.route("/").get(inventoryController.index);
 router.route("/").get(inventoryController.getAllInventories);
-router.put("/:id", inventoryController.updateInventory);
+router.route("/").post(inventoryController.upsertItem);
+router.put("/:id", inventoryController.upsertItem);
 
 // api/inventories/:id
 router.route("/inventory/:id").get(inventoryController.singleItem);
